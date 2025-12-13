b3d4816b725124c2f1f9c8f755174ad89f7f3cfc
Batch1 <Batch1@VITUNIVERSITY.LOCAL>
2025-12-13 09:21:27 +0530

    version2

--- fibonacci.c
+++ fibonacci.c
@@ -2,20 +2,27 @@
 
 int main() {
     int n, i;
-    unsigned long long t1 = 0, t2 = 1, nextTerm;
+    unsigned long long t1 = 0, t2 = 1, nextTerm, sum = 0;
 
     printf("Enter the number of terms: ");
     scanf("%d", &n);
 
-    printf("Fibonacci Series: ");
+    if (n <= 0) {
+        printf("Error! Please enter a positive integer.\n");
+        return 1;
+    }
+
+    printf("Fibonacci Series up to %d terms:\n", n);
 
     for (i = 1; i <= n; ++i) {
         printf("%llu ", t1);
+        sum += t1;  // keep track of sum
         nextTerm = t1 + t2;
         t1 = t2;
         t2 = nextTerm;
     }
 
-    printf("\n");
+    printf("\nSum of the series = %llu\n", sum);
+
     return 0;
 }
